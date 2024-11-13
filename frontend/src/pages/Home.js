ccafc39de190c407b3dbae99ee4544b29866d329
Priyadharshini K <86607144+Priyapk56@users.noreply.github.com>
2024-11-13 13:16:37 +0530

    project completed

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,37 @@
+import {Fragment, useEffect, useState} from 'react';
+import ProductCard from '../components/ProductCard';
+import { useSearchParams } from 'react-router-dom';
+
+export default function Home() 
+{
+    const[products,setProducts]=useState([]);
+    const [searchParams] =  useSearchParams();
+
+
+    useEffect(() => {
+        fetch(process.env.REACT_APP_API_URL + '/products?'+searchParams)
+            .then(res => res.json())
+            .then(res => {
+                setProducts(res.products);
+            })
+            .catch(err => {
+                console.error('Error fetching products:', err);
+            });
+    }, [searchParams]);
+
+    return <Fragment>
+
+    <h1 id="products_heading">Latest Products</h1>
+
+    <section id="products" className="container mt-5">
+      <div className="row">
+        {products.map(product=> <ProductCard key={product._id} product={product}/>)}
+        
+
+      </div>
+    </section>
+
+    
+        
+    </Fragment>
+}
